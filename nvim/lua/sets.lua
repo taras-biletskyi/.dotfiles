7c4b23dd08607f1886f7e81e670bffb21903f6eb
taras-biletskyi <tarasbiletskyi@gmail.com>
2023-09-03 13:47:47 +0300

    fix(nvim): set spell only for markdown buffers

--- nvim/lua/sets.lua
+++ nvim/lua/sets.lua
@@ -71,7 +71,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 vim.api.nvim_create_autocmd("FileType", {
     pattern = {"gitcommit", "markdown"},
-    callback = function() vim.cmd('set spell') end
+    callback = function() vim.cmd('setlocal spell') end
 })
 vim.opt.wrap = false
 vim.o.smarttab = true
