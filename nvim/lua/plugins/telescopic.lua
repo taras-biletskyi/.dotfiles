9c94e3c462b65312306a098ce82a4fdef774b64e
taras-biletskyi <tarasbiletskyi@gmail.com>
2023-09-03 13:47:46 +0300

    feat: beautified telescopic

--- nvim/lua/plugins/telescopic.lua
+++ nvim/lua/plugins/telescopic.lua
@@ -18,15 +18,20 @@ function _G.project_files()
     end
 end
 
-local ivy_theme = require('telescope.themes').get_ivy({show_untracked = true})
-vim.keymap.set('n', '<leader>tf', project_files)
-vim.keymap.set('n', '<leader>tg', function() Builtin.live_grep(ivy_theme) end, {})
-vim.keymap.set('n', '<leader>tb', function() Builtin.buffers(ivy_theme) end, {})
-vim.keymap.set('n', '<leader>td', function() Builtin.git_files(require('telescope.themes').get_ivy({prompt_title = ".dotfiles", cwd = "~/.dotfiles/", show_untracked = true})) end, {})
-vim.keymap.set('n', '<leader>th', function() Builtin.help_tags(ivy_theme) end, {})
-vim.keymap.set('n', '<leader>te', function() Builtin.symbols(ivy_theme) end, {}) -- emoji
+require "telescope".setup{defaults = {dynamic_preview_title = true}}
 
-vim.keymap.set('n', '<leader>tls', function() Builtin.lsp_dynamic_workspace_symbols(ivy_theme) end, {})
-vim.keymap.set('n', '<leader>tlr', function() Builtin.lsp_references(ivy_theme) end, {})
-vim.keymap.set('n', '<leader>tli', function() Builtin.lsp_incoming_calls(ivy_theme) end, {})
-vim.keymap.set('n', '<leader>tlo', function() Builtin.lsp_outgoing_calls(ivy_theme) end, {})
+local opts = {}
+opts = require("telescope.themes").get_ivy{show_untracked = true}
+opts.path_display = {shorten = {len = 3, exclude = {1, -1}}, truncate = true}
+
+vim.keymap.set('n', '<leader>tf', project_files)
+vim.keymap.set('n', '<leader>tg', function() Builtin.live_grep(opts) end, {})
+vim.keymap.set('n', '<leader>tb', function() Builtin.buffers(opts) end, {})
+vim.keymap.set('n', '<leader>td', function() Builtin.git_files(require('telescope.themes').get_ivy({prompt_title = ".dotfiles", cwd = "~/.dotfiles/", show_untracked = true})) end, {})
+vim.keymap.set('n', '<leader>th', function() Builtin.help_tags(opts) end, {})
+vim.keymap.set('n', '<leader>te', function() Builtin.symbols(opts) end, {}) -- emoji
+
+vim.keymap.set('n', '<leader>tls', function() Builtin.lsp_dynamic_workspace_symbols(opts) end, {})
+vim.keymap.set('n', '<leader>tlr', function() Builtin.lsp_references(opts) end, {})
+vim.keymap.set('n', '<leader>tli', function() Builtin.lsp_incoming_calls(opts) end, {})
+vim.keymap.set('n', '<leader>tlo', function() Builtin.lsp_outgoing_calls(opts) end, {})
