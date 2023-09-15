261ceff7cf686a44cb1be988897e0eaea2c0a2b0
taras-biletskyi <tarasbiletskyi@gmail.com>
2023-09-15 15:21:53 +0300

    fix(nvim): remove some tree_textobjects stuff

--- nvim/lua/plugins/tree_textobjects.lua
+++ nvim/lua/plugins/tree_textobjects.lua
@@ -62,17 +62,18 @@ require'nvim-treesitter.configs'.setup {
     }
 }
 
-local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+-- local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
 
 -- Repeat movement with ; and ,
 -- vim way: ; goes to the direction you were moving.
-vim.keymap.set({"n", "x", "o"}, ";", ts_repeat_move.repeat_last_move)
-vim.keymap.set({"n", "x", "o"}, ",", ts_repeat_move.repeat_last_move_opposite)
-
-vim.keymap.set({"n", "x", "o"}, "f", ts_repeat_move.builtin_f)
-vim.keymap.set({"n", "x", "o"}, "F", ts_repeat_move.builtin_F)
-vim.keymap.set({"n", "x", "o"}, "t", ts_repeat_move.builtin_t)
-vim.keymap.set({"n", "x", "o"}, "T", ts_repeat_move.builtin_T)
+-- These mess with native vim reapeats!!!
+-- vim.keymap.set({"n", "x", "o"}, ";", ts_repeat_move.repeat_last_move)
+-- vim.keymap.set({"n", "x", "o"}, ",", ts_repeat_move.repeat_last_move_opposite)
+--
+-- vim.keymap.set({"n", "x", "o"}, "f", ts_repeat_move.builtin_f)
+-- vim.keymap.set({"n", "x", "o"}, "F", ts_repeat_move.builtin_F)
+-- vim.keymap.set({"n", "x", "o"}, "t", ts_repeat_move.builtin_t)
+-- vim.keymap.set({"n", "x", "o"}, "T", ts_repeat_move.builtin_T)
 
 -- -- INFO: for gitsigns plugin
 -- local gs = require("gitsigns")
