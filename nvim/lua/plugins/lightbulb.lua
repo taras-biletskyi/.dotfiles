ada83e8429e9e59179efb3e85ca9d0b89ef9223d
taras-biletskyi <tarasbiletskyi@gmail.com>
2023-10-30 21:18:55 +0200

    fix(nvim): lightbulb change

--- nvim/lua/plugins/lightbulb.lua
+++ nvim/lua/plugins/lightbulb.lua
@@ -2,7 +2,7 @@ function update_lightbulb()
     require"nvim-lightbulb".update_lightbulb {
         sign = {enabled = false},
         float = {enabled = false},
-        virtual_text = {enabled = true, text = "💡", hl_mode = "combine"}
+        virtual_text = {enabled = true, text = "", hl = "Comment", hl_mode = "combine"}
     }
 end
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua update_lightbulb()]]
