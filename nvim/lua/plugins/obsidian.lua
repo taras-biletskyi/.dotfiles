e397c9347cb57028cd3cca7b637e7eba204baa73
taras-biletskyi <tarasbiletskyi@gmail.com>
2024-01-24 08:48:03 +0200

    feat(nvim): obsidian

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -2,7 +2,7 @@ require("obsidian").setup({
     lazy = true,
     dir = "~/personal/fault",
     disable_frontmatter = true,
-    ui = {enable = false},
+    ui = {enable = true},
     daily_notes = {
         -- Optional, if you keep daily notes in a separate directory.
         folder = "daily",
