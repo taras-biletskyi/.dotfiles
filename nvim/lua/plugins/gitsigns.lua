0f53c379444c0b308d22e1215c1d00d401df808d
taras-biletskyi <tarasbiletskyi@gmail.com>
2024-01-26 10:29:23 +0200

    fix(nvim): breaking change upstream for gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -13,6 +13,7 @@ require('gitsigns').setup {
   word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
   current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  attach_to_untracked = true,
 
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
