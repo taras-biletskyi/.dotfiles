07f53f8e706c7f6fcc9e3886ed0fcc8d5e98c58f
taras-biletskyi <tarasbiletskyi@gmail.com>
2024-02-07 12:01:34 +0200

    feat(nvim): awesome telescope keymaps, fzf-like

--- nvim/lua/plugins/telescopic.lua
+++ nvim/lua/plugins/telescopic.lua
@@ -1,5 +1,21 @@
 local Builtin = require("telescope.builtin")
 local actions = require("telescope.actions")
+
+local select_one_or_multi = function(prompt_bufnr)
+	local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+	local multi = picker:get_multi_selection()
+	if not vim.tbl_isempty(multi) then
+		require("telescope.actions").close(prompt_bufnr)
+		for _, j in pairs(multi) do
+			if j.path ~= nil then
+				vim.cmd(string.format("%s %s", "edit", j.path))
+			end
+		end
+	else
+		require("telescope.actions").select_default(prompt_bufnr)
+	end
+end
+
 require("telescope").setup({
 	defaults = {
 		dynamic_preview_title = true,
@@ -21,12 +37,24 @@ require("telescope").setup({
 				["<M-q>"] = false,
 				["<C-q>a"] = actions.send_to_qflist + actions.open_qflist,
 				["<C-q>q"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<CR>"] = select_one_or_multi,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-Space>"] = actions.toggle_selection + actions.move_selection_next,
+				["<Tab>"] =  actions.move_selection_next,
+				["<S-Tab>"] =  actions.move_selection_previous,
 			},
 			i = {
 				["<C-q>"] = false,
 				["<M-q>"] = false,
 				["<C-q>a"] = actions.send_to_qflist + actions.open_qflist,
 				["<C-q>q"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<CR>"] = select_one_or_multi,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-Space>"] = actions.toggle_selection + actions.move_selection_next,
+				["<Tab>"] =  actions.move_selection_next,
+				["<S-Tab>"] =  actions.move_selection_previous,
 			},
 		},
 	},
