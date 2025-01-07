215c4e48275911f71269ab94ed2c80a0d0205e66
taras-biletskyi <tarasbiletskyi@gmail.com>
2025-01-07 21:40:16 +0200

    feat(neovim): add gitlab MR plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -25,6 +25,22 @@ require("lazy").setup({
 		version = "^2",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
+	{
+		"harrisoncramer/gitlab.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			-- "nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
+		},
+		enabled = true,
+		build = function()
+			require("gitlab.server").build(true)
+		end,
+		config = function()
+			require("gitlab").setup()
+		end,
+	},
 	"nvim-lua/plenary.nvim",
 	"sindrets/diffview.nvim",
 	-- ctags thing for code outline
