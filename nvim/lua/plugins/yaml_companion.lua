7325ce422e9a153965d60e9263bbcb926c997d00
taras-biletskyi <tarasbiletskyi@gmail.com>
2025-04-03 10:52:08 +0300

    feat: make yaml-companion and yamlls work with custom schemas

--- nvim/lua/plugins/yaml_companion.lua
+++ nvim/lua/plugins/yaml_companion.lua
@@ -1,9 +1,87 @@
+local strimzi_schemas = {
+	{
+		name = "Kafka",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafka_v1beta2.json",
+	},
+	{
+		name = "KafkaBridge",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkabridge_v1beta2.json",
+	},
+	{
+		name = "KafkaConnector",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkaconnector_v1beta2.json",
+	},
+	{
+		name = "KafkaConnect_v1beta2",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkaconnect_v1beta2.json",
+	},
+	{
+		name = "KafkaMirrorMaker",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkaconnector_v1beta2.json",
+	},
+	{
+		name = "KafkaNodePool",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkanodepool_v1beta2.json",
+	},
+	{
+		name = "KafkaMirrorMaker2",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkaconnector_v1beta2.json",
+	},
+	{
+		name = "KafkaRebalance",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkarebalance_v1beta2.json",
+	},
+	{
+		name = "KafkaTopic_v1alpha1",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkatopic_v1alpha1.json",
+	},
+	{
+		name = "KafkaTopic_v1beta1",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkatopic_v1beta1.json",
+	},
+	{
+		name = "KafkaTopic_v1beta2",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkatopic_v1beta2.json",
+	},
+	{
+		name = "KafkaTopicControlsACLs_v1alpha1",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkatopiccontrolacls_v1alpha1.json",
+	},
+	{
+		name = "KafkaTopicControlsACLs_v1beta1",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkatopiccontrolacls_v1beta1.json",
+	},
+	{
+		name = "KafkaUser_v1alpha1",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkauser_v1alpha1.json",
+	},
+	{
+		name = "KafkaUser_v1beta1",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkauser_v1beta1.json",
+	},
+	{
+		name = "KafkaUser_v1beta2",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/kafkauser_v1beta2.json",
+	},
+	{
+		name = "StrimziPodSet",
+		uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/kafka.strimzi.io/strimzipodset_v1beta2.json",
+	},
+}
+
+local schemas = {}
+
+for _, schema in ipairs(strimzi_schemas) do
+	table.insert(schemas, {
+		name = "kafka.strimzi.io - " .. schema.name,
+		uri = schema.uri,
+	})
+end
+
 local cfg = require("yaml-companion").setup({
-	-- Add any options here, or leave empty to use the default settings
-	-- lspconfig = {
-	--   cmd = {"yaml-language-server"}
-	-- },
+	schemas = schemas,
 })
+
 require("lspconfig")["yamlls"].setup(cfg)
 
 -- Get the schema name for the current buffer 👇
