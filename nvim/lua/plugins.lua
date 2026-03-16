7d99a3cff33e6aba5a082a418b6c272edeb60829
taras-biletskyi <tarasbiletskyi@gmail.com>
2026-03-16 23:25:09 +0200

    fix(nvim): remove rest plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -171,16 +171,6 @@ require("lazy").setup({
 			end,
 		},
 		{
-			"rest-nvim/rest.nvim",
-			dependencies = {
-				"nvim-treesitter/nvim-treesitter",
-				opts = function(_, opts)
-					opts.ensure_installed = opts.ensure_installed or {}
-					table.insert(opts.ensure_installed, "http")
-				end,
-			},
-		},
-		{
 			"zbirenbaum/copilot.lua",
 		},
 	},
